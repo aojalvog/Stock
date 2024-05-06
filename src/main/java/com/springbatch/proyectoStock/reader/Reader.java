5db0c32e7693a60db6720a996d09e5a570da6e63
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-06 17:46:53 +0200

    feat(Stock): :sparkles: Creo el Reader

--- src/main/java/com/springbatch/proyectoStock/reader/Reader.java
+++ src/main/java/com/springbatch/proyectoStock/reader/Reader.java
@@ -1,42 +1,48 @@
 package com.springbatch.proyectoStock.reader;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
 import com.springbatch.proyectoStock.model.Stock;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+// Importa el LOGGER
+@Slf4j
 public class Reader {
 
-	public static final Logger log = LoggerFactory.getLogger(Reader.class);
-
-	@Value("${app.file.input}")
-	private String stockFile;
-
-	// Lectura del dat de stockTerminales.dat
-	@Bean
 	public FlatFileItemReader<Stock> reader() {
-		log.info("Leyendo fichero...");
-		return new FlatFileItemReaderBuilder<Stock>().name("stockItemReader").resource(new ClassPathResource(stockFile))
-				.delimited().names("lugar", "id", "stock", "stockReal", "stockVirtual")
-				.fieldSetMapper(new BeanWrapperFieldSetMapper<Stock>() {
-					{
-						setTargetType(Stock.class);
-					}
-				}).linesToSkip(1) // salta la primera línea, que es la cabecera
-				.build();
-	}
-}
 
-/*
- * @Override public List<Stock> read() { List<Stock> stockFiltrado = new
- * ArrayList<>(); log.info("Leyendo fichero"); for (Stock stock : stockFiltrado)
- * { if (stock.getLugar().equals("PENINSULA")) { stockFiltrado.add(stock); } }
- * 
- * return stockFiltrado; }
- */
+		FlatFileItemReader<Stock> reader = new FlatFileItemReader<>();
+		// Selecciona la ruta del archivo que va a leer
+		reader.setResource(new ClassPathResource("stockTerminales.dat"));
+		// Skipeo una línea porque la primera línea del documento es la cabezera.
+		reader.setLinesToSkip(1);
+		reader.setLineMapper(lineMapper());
+		return reader;
+	}
+
+	private LineMapper<Stock> lineMapper() {
+		DefaultLineMapper<Stock> lineMapper = new DefaultLineMapper<>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+		// Te dice lo que separa los diferentes puntos
+		lineTokenizer.setDelimiter(";");
+		lineTokenizer.setNames("lugar", "id", "stockTotal", "stockReal", "stockVirtual");
+
+		BeanWrapperFieldSetMapper<Stock> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		// Esto es a lo que vamos a convertir
+		fieldSetMapper.setTargetType(Stock.class);
+
+		lineMapper.setLineTokenizer(lineTokenizer);
+		lineMapper.setFieldSetMapper(fieldSetMapper);
+
+		return lineMapper;
+	}
+
+}
